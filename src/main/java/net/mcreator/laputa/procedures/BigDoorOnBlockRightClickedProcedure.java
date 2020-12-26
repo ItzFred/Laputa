86788964938fe09aeba45c96396b6544546a047e
ItzFred <Fred Brandibur@Fred>
2020-12-26 19:44:31 +0100

    Room Spawning Procedures

---
+++ src/main/java/net/mcreator/laputa/procedures/BigDoorOnBlockRightClickedProcedure.java
@@ -0,0 +1,179 @@
+package net.mcreator.laputa.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.gen.feature.template.Template;
+import net.minecraft.world.gen.feature.template.PlacementSettings;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Direction;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.laputa.LaputaModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@LaputaModElements.ModElement.Tag
+public class BigDoorOnBlockRightClickedProcedure extends LaputaModElements.ModElement {
+	public BigDoorOnBlockRightClickedProcedure(LaputaModElements instance) {
+		super(instance, 130);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				System.err.println("Failed to load dependency x for procedure BigDoorOnBlockRightClicked!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				System.err.println("Failed to load dependency y for procedure BigDoorOnBlockRightClicked!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				System.err.println("Failed to load dependency z for procedure BigDoorOnBlockRightClicked!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				System.err.println("Failed to load dependency world for procedure BigDoorOnBlockRightClicked!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if (((new Object() {
+			public Direction getDirection(BlockPos pos) {
+				try {
+					BlockState _bs = world.getBlockState(pos);
+					DirectionProperty property = (DirectionProperty) _bs.getBlock().getStateContainer().getProperty("facing");
+					return _bs.get(property);
+				} catch (Exception e) {
+					return Direction.NORTH;
+				}
+			}
+		}.getDirection(new BlockPos((int) x, (int) y, (int) z))) == Direction.SOUTH)) {
+			if ((BlockTags.getCollection().getOrCreate(new ResourceLocation(("forge:dungeon_blocks").toLowerCase(java.util.Locale.ENGLISH)))
+					.contains((world.getBlockState(new BlockPos((int) x, (int) (y - 1), (int) (z - 2)))).getBlock()))) {
+				if (!world.getWorld().isRemote) {
+					Template template = ((ServerWorld) world.getWorld()).getSaveHandler().getStructureTemplateManager()
+							.getTemplateDefaulted(new ResourceLocation("laputa", "testglacialisroomnodoors"));
+					if (template != null) {
+						template.addBlocksToWorld(world, new BlockPos((int) (x - 15), (int) (y - 1), (int) (z - 31)),
+								new PlacementSettings().setRotation(Rotation.NONE).setMirror(Mirror.NONE).setChunk(null).setIgnoreEntities(false));
+					}
+				}
+			} else {
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("world", world);
+					$_dependencies.put("x", (x - 15));
+					$_dependencies.put("y", (y - 1));
+					$_dependencies.put("z", (z - 31));
+					GlacialisRoomSpawnProcedure.executeProcedure($_dependencies);
+				}
+			}
+		} else if (((new Object() {
+			public Direction getDirection(BlockPos pos) {
+				try {
+					BlockState _bs = world.getBlockState(pos);
+					DirectionProperty property = (DirectionProperty) _bs.getBlock().getStateContainer().getProperty("facing");
+					return _bs.get(property);
+				} catch (Exception e) {
+					return Direction.NORTH;
+				}
+			}
+		}.getDirection(new BlockPos((int) x, (int) y, (int) z))) == Direction.NORTH)) {
+			if ((BlockTags.getCollection().getOrCreate(new ResourceLocation(("forge:dungeon_blocks").toLowerCase(java.util.Locale.ENGLISH)))
+					.contains((world.getBlockState(new BlockPos((int) x, (int) (y - 1), (int) (z + 2)))).getBlock()))) {
+				if (!world.getWorld().isRemote) {
+					Template template = ((ServerWorld) world.getWorld()).getSaveHandler().getStructureTemplateManager()
+							.getTemplateDefaulted(new ResourceLocation("laputa", "testglacialisroomnodoors"));
+					if (template != null) {
+						template.addBlocksToWorld(world, new BlockPos((int) (x - 16), (int) (y - 1), (int) z),
+								new PlacementSettings().setRotation(Rotation.NONE).setMirror(Mirror.NONE).setChunk(null).setIgnoreEntities(false));
+					}
+				}
+			} else {
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("world", world);
+					$_dependencies.put("x", (x - 16));
+					$_dependencies.put("y", (y - 1));
+					$_dependencies.put("z", z);
+					GlacialisRoomSpawnProcedure.executeProcedure($_dependencies);
+				}
+			}
+		} else if (((new Object() {
+			public Direction getDirection(BlockPos pos) {
+				try {
+					BlockState _bs = world.getBlockState(pos);
+					DirectionProperty property = (DirectionProperty) _bs.getBlock().getStateContainer().getProperty("facing");
+					return _bs.get(property);
+				} catch (Exception e) {
+					return Direction.NORTH;
+				}
+			}
+		}.getDirection(new BlockPos((int) x, (int) y, (int) z))) == Direction.EAST)) {
+			if ((BlockTags.getCollection().getOrCreate(new ResourceLocation(("forge:dungeon_blocks").toLowerCase(java.util.Locale.ENGLISH)))
+					.contains((world.getBlockState(new BlockPos((int) (x - 2), (int) (y - 1), (int) z))).getBlock()))) {
+				if (!world.getWorld().isRemote) {
+					Template template = ((ServerWorld) world.getWorld()).getSaveHandler().getStructureTemplateManager()
+							.getTemplateDefaulted(new ResourceLocation("laputa", "testglacialisroomnodoors"));
+					if (template != null) {
+						template.addBlocksToWorld(world, new BlockPos((int) (x - 31), (int) (y - 1), (int) (z - 16)),
+								new PlacementSettings().setRotation(Rotation.NONE).setMirror(Mirror.NONE).setChunk(null).setIgnoreEntities(false));
+					}
+				}
+			} else {
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("world", world);
+					$_dependencies.put("x", (x - 31));
+					$_dependencies.put("y", (y - 1));
+					$_dependencies.put("z", (z - 16));
+					GlacialisRoomSpawnProcedure.executeProcedure($_dependencies);
+				}
+			}
+		} else if (((new Object() {
+			public Direction getDirection(BlockPos pos) {
+				try {
+					BlockState _bs = world.getBlockState(pos);
+					DirectionProperty property = (DirectionProperty) _bs.getBlock().getStateContainer().getProperty("facing");
+					return _bs.get(property);
+				} catch (Exception e) {
+					return Direction.NORTH;
+				}
+			}
+		}.getDirection(new BlockPos((int) x, (int) y, (int) z))) == Direction.WEST)) {
+			if ((BlockTags.getCollection().getOrCreate(new ResourceLocation(("forge:dungeon_blocks").toLowerCase(java.util.Locale.ENGLISH)))
+					.contains((world.getBlockState(new BlockPos((int) (x + 2), (int) (y - 1), (int) z))).getBlock()))) {
+				if (!world.getWorld().isRemote) {
+					Template template = ((ServerWorld) world.getWorld()).getSaveHandler().getStructureTemplateManager()
+							.getTemplateDefaulted(new ResourceLocation("laputa", "testglacialisroomnodoors"));
+					if (template != null) {
+						template.addBlocksToWorld(world, new BlockPos((int) x, (int) (y - 1), (int) (z - 15)),
+								new PlacementSettings().setRotation(Rotation.NONE).setMirror(Mirror.NONE).setChunk(null).setIgnoreEntities(false));
+					}
+				}
+			} else {
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("world", world);
+					$_dependencies.put("x", x);
+					$_dependencies.put("y", (y - 1));
+					$_dependencies.put("z", (z - 15));
+					GlacialisRoomSpawnProcedure.executeProcedure($_dependencies);
+				}
+			}
+		}
+		world.destroyBlock(new BlockPos((int) x, (int) y, (int) z), false);
+	}
+}
---
+++ src/main/java/net/mcreator/laputa/procedures/DoorBreakProcedureProcedure.java
@@ -0,0 +1,62 @@
+package net.mcreator.laputa.procedures;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.laputa.LaputaModElements;
+
+import java.util.stream.Collectors;
+import java.util.function.Function;
+import java.util.Map;
+import java.util.List;
+import java.util.Comparator;
+
+@LaputaModElements.ModElement.Tag
+public class DoorBreakProcedureProcedure extends LaputaModElements.ModElement {
+	public DoorBreakProcedureProcedure(LaputaModElements instance) {
+		super(instance, 136);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				System.err.println("Failed to load dependency x for procedure DoorBreakProcedure!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				System.err.println("Failed to load dependency y for procedure DoorBreakProcedure!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				System.err.println("Failed to load dependency z for procedure DoorBreakProcedure!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				System.err.println("Failed to load dependency world for procedure DoorBreakProcedure!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		{
+			List<Entity> _entfound = world
+					.getEntitiesWithinAABB(Entity.class,
+							new AxisAlignedBB(x - (4 / 2d), y - (4 / 2d), z - (4 / 2d), x + (4 / 2d), y + (4 / 2d), z + (4 / 2d)), null)
+					.stream().sorted(new Object() {
+						Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+							return Comparator.comparing((Function<Entity, Double>) (_entcnd -> _entcnd.getDistanceSq(_x, _y, _z)));
+						}
+					}.compareDistOf(x, y, z)).collect(Collectors.toList());
+			for (Entity entityiterator : _entfound) {
+				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), Blocks.AIR.getDefaultState(), 3);
+			}
+		}
+	}
+}
